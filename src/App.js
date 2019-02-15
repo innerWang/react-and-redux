4613a7edd560cd30533071a34b1f7d81d5e58f81
unknown <52886377@qq.com>
2019-02-15 16:09:53 +0800

    react+redux+react-redux

--- src/App.js
+++ src/App.js
@@ -1,38 +1,104 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+
+// class App extends Component {
+
+//   add1 = ()=>{
+//     this.props.onAdd1()
+//   }
+
+//   add2 = ()=>{
+//     this.props.onAdd2()
+//   }
+
+//   add3 = ()=>{
+//     this.props.onAdd3()
+//   }
+
+//   add4(){
+//     this.props.onAdd4()
+//   }
+
+
+//   render() {
+//     return (
+//       <div className="App">
+//         你点击了<span id="value">{this.props.value}</span>次
+//         <div>
+//           <button id="add1" onClick = {this.add1}>+1</button>
+//           <button id="add2" onClick = {this.add2}>+2</button>
+//           <button id="addIfOdd" onClick = {this.add3}>如果是单数就+1</button>
+//           <button id="addAfterTwoSec" onClick = {()=>this.add4()}>两秒钟后+1</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+//export default App;
 
 class App extends Component {
 
-  add1 = ()=>{
-    this.props.onAdd1()
-  }
-
-  add2 = ()=>{
-    this.props.onAdd2()
-  }
-
-  add3 = ()=>{
-    this.props.onAdd3()
-  }
-
-  add4(){
-    this.props.onAdd4()
-  }
-
-
-  render() {
-    return (
-      <div className="App">
-        你点击了<span id="value">{this.props.value}</span>次
+  render(){
+    return (<div>
+      你点击了<span id="value">{this.props.n}</span>次
         <div>
-          <button id="add1" onClick = {this.add1}>+1</button>
-          <button id="add2" onClick = {this.add2}>+2</button>
-          <button id="addIfOdd" onClick = {this.add3}>如果是单数就+1</button>
-          <button id="addAfterTwoSec" onClick = {()=>this.add4()}>两秒钟后+1</button>
-        </div>
-      </div>
-    );
+           <button id="add1" onClick = {this.props.add1}>+1</button>
+           <button id="add2" onClick = {this.props.add2}>+2</button>
+           <button id="addIfOdd" onClick = {this.props.add3}>如果是单数就+1</button>
+           <button id="addAfterTwoSec" onClick = {this.props.add4}>两秒钟后+1</button>
+         </div>
+    </div>)
   }
 }
 
-export default App;
+//map state to props ,用于获取部分store,即将state里面的内容传给组件的props
+function x(state){
+  return {
+    n:state.n
+  }
+}
+
+// map Dispatch To Props,用于生成action,是一个对象
+// const y= {
+//     add1: ()=>{
+//       return {type:'add',payload:1}
+//     },
+//     add2: ()=>{
+//       return {type:'add',payload:2}
+//     },
+//     add3: (content)=>{
+//       if(content % 2 === 1){
+//         return {type:'add',payload:1}
+//       }
+//     },
+//     add4: ()=>{
+//       //setTimeout(()=>{return {type:'add',payload:1}},2000)
+//     }
+// }
+
+//将mapDispatch写成函数
+function y(dispatch){
+  return {
+    add1: ()=>{
+      dispatch({type:'add',payload:1}) 
+    },
+    add2: ()=>{
+      dispatch({type:'add',payload:2}) 
+    },
+    add3: ()=>{
+      
+    },
+    add4: ()=>{
+      
+    }
+  }
+}
+
+
+
+
+export default connect(x,y)(App)
+
+
--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
+import { Provider } from 'react-redux'
 
 const stateChanger = (state,action)=>{
   if(state === undefined){
-    return 0
+    return {n:0}
   }else{
     if(action.type ==='add'){
-      var newState = state + action.payload
-      console.log(newState)
-      return newState
+      let newstate = {...state,n:(state.n+action.payload)}
+      return newstate
     }else{
       return state
     }
@@ -20,30 +20,35 @@ const stateChanger = (state,action)=>{
 }
 
 const store = createStore(stateChanger);
-render()
-store.subscribe(()=>render());
+// render()
+// store.subscribe(()=>render());
 
 
-function add3(){
-  if(store.getState()%2 ===1){
-    store.dispatch({type:'add',payload:1})
-  }
-}
+// function add3(){
+//   if(store.getState()%2 ===1){
+//     store.dispatch({type:'add',payload:1})
+//   }
+// }
 
-function add4(){
-  setTimeout(()=>{
-    store.dispatch({type:'add',payload:1})
-  },2000)
-}
+// function add4(){
+//   setTimeout(()=>{
+//     store.dispatch({type:'add',payload:1})
+//   },2000)
+// }
 
-function render(){
-  ReactDOM.render(<App value={store.getState()}
-  onAdd1 = {()=>{store.dispatch({type:'add',payload:1})}}
-  onAdd2 = {()=>{store.dispatch({type:'add',payload:2})}}
-  onAdd3 = {add3}
-  onAdd4 = {add4}
-/>, document.getElementById('root'));
-}
+// function render(){
+//   ReactDOM.render(<App value={store.getState()}
+//   onAdd1 = {()=>{store.dispatch({type:'add',payload:1})}}
+//   onAdd2 = {()=>{store.dispatch({type:'add',payload:2})}}
+//   onAdd3 = {add3}
+//   onAdd4 = {add4}
+// />, document.getElementById('root'));
+// }
+
+
+  ReactDOM.render(<Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
